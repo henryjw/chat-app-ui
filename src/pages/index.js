048fef776cb894f4a73bd6189962051072798ce9
Henry Williams <3443648+henryjw@users.noreply.github.com>
2020-04-11 19:08:46 -0400

    add login skeleton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
 import React from "react"
-import { Button, FormLabel, FormControl, Input, FormGroup } from '@material-ui/core'
+import { Button, FormLabel, Input, FormGroup } from '@material-ui/core'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import login from "../services/login"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Login" />
-    <FormGroup>
-      <FormLabel htmlFor="#username">Username</FormLabel>
-      <Input id="username"></Input>
-      <Button>Ok</Button>
-    </FormGroup>
-  </Layout>
-)
+const IndexPage = () => {
+  let username = null;
+  return (
+    <Layout>
+      <SEO title="Login" />
+      <FormGroup>
+        <FormLabel htmlFor="#username">Username</FormLabel>
+        <Input id="username" onChange={e => username = e.target.value}></Input>
+        <Button onClick={() => login(username)}>Ok</Button>
+      </FormGroup>
+    </Layout>
+  )
+}
 
 export default IndexPage
---
+++ src/services/login.js
@@ -0,0 +1,8 @@
+export default async function login(username) {
+    // 1) Check if username is available [2nd iteration]
+    //  1a) If username not available, throw an error [2nd iteration] 
+    // 2) Save username in the database [2nd iteration]
+    // 3) Save username in local memory [1st iteration]
+    
+    throw new Error("Not yet implemented");
+}
