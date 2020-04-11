3d300efa69c12cea577a31ee899f67392115f488
Henry Williams <3443648+henryjw@users.noreply.github.com>
2020-04-11 18:49:46 -0400

    update home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chat App`,
+    description: `Simple chat app`,
+    author: `@henryjw`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Button, FormLabel, FormControl, Input, FormGroup } from '@material-ui/core'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Login" />
+    <FormGroup>
+      <FormLabel htmlFor="#username">Username</FormLabel>
+      <Input id="username"></Input>
+      <Button>Ok</Button>
+    </FormGroup>
   </Layout>
 )
 
