446e83a40bdbadf326ede10ef2c4b64b0d826822
Henry Williams <3443648+henryjw@users.noreply.github.com>
2020-04-26 15:04:43 -0400

    auto focus on username

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import { navigate } from 'gatsby'
 import { Button, FormLabel, Input, FormGroup } from '@material-ui/core'
 
@@ -24,11 +24,11 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Login" />
       <form onSubmit={e => { e.preventDefault(); handleLogin(); }}>
-       <FormGroup>
+        <FormGroup>
           <FormLabel htmlFor="#username">Username</FormLabel>
-          <Input id="username" onChange={e => setUsername(e.target.value)}></Input>
+          <Input id="username" autoFocus={true} onChange={e => setUsername(e.target.value)}></Input>
           <Button type="submit">Ok</Button>
-       </FormGroup>
+        </FormGroup>
       </form>
     </Layout>
   )
